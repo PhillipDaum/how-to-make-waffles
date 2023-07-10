1ad59d56190273aa958d76c85c07e828dbd59d1f
Phil Daum <philgdaum@gmail.com>
2023-07-10 13:50:39 -0700

    fix user.js, change waffle certificate to png

--- story_content/user.js
+++ story_content/user.js
@@ -13,13 +13,13 @@ function ExecuteScript(strId)
 
 function Script1()
 {
-  var recipe = "how-to-make-waffles/docs/yummy-waffles.pdf";
+  var recipe = "./docs/yummy-waffles.pdf";
   window.open(recipe);
 }
 
 function Script2()
 {
-  var date = new Date();
+var date = new Date();
 var dd = String(date.getDate()).padStart(2, '0');
 var mm = String(date.getMonth() + 1).padStart(2, '0'); 
 var yyyy = date.getFullYear();
@@ -39,16 +39,16 @@ doc.setTextColor(0, 0, 0);
 doc.setFont('Times-Bold');
 
 /* coordinates */
-doc.text(name, (doc.internal.pageSize.width / 2), 120, null, null, 'center'); 
+doc.text(name, (doc.internal.pageSize.width / 2), 137, null, null, 'center'); 
 doc.setFont('Times-Roman');
 doc.setFontSize(15);
-doc.text(date, (doc.internal.pageSize.width / 2), 172, null, null, 'center'); 
+doc.text(date, (doc.internal.pageSize.width / 2), 146, null, null, 'center'); 
 /* name */
 doc.save("i-made-waffles.pdf"); 
 };
 
 /* source */
 img.crossOrigin = "";  
-img.src = "how-to-make-waffles/docs/waffles.pdf"; 
+img.src = "./docs/waffles.png"; 
 }
 
